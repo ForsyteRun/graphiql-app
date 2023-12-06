11cfa77a889ab2810681ada8efff8d0113d2049b
massaracsh7 <fatum7@gmail.com>
2023-12-06 10:03:31 +0300

    feat: add login, register, logout functions

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,15 +1,13 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { FirebaseError, initializeApp } from 'firebase/app';
 import {
-  //createUserWithEmailAndPassword,
-  //signInWithEmailAndPassword,
-  //signOut,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
   getAuth,
 } from 'firebase/auth';
 
-// Your web app's Firebase configuration
+import { getFirestore, collection, addDoc } from 'firebase/firestore';
+
 const firebaseConfig = {
   apiKey: 'AIzaSyA8WOnUC6pRs_wi-b40z264_qWf3fBTXh4',
   authDomain: 'graphiql-reactmaniacs.firebaseapp.com',
@@ -19,8 +17,47 @@ const firebaseConfig = {
   appId: '1:295355622156:web:bf1c5f8c473094b99edf29',
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app);
 
-export { auth };
+const logInWithEmailAndPassword = async (email: string, password: string) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (err) {
+    if (err instanceof FirebaseError) {
+      console.error(err.code);
+    }
+  }
+};
+const registerWithEmailAndPassword = async (
+  name: string,
+  email: string,
+  password: string
+) => {
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password);
+    const user = res.user;
+    await addDoc(collection(db, 'users'), {
+      uid: user.uid,
+      name,
+      authProvider: 'local',
+      email,
+    });
+  } catch (err) {
+    if (err instanceof FirebaseError) {
+      console.error(err.code);
+    }
+  }
+};
+
+const logout = () => {
+  signOut(auth);
+};
+export {
+  auth,
+  db,
+  logInWithEmailAndPassword,
+  registerWithEmailAndPassword,
+  logout,
+};
