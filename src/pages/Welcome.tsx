21344b45e3f73480a1fe6c3de3bb9485e1730103
ForsyteRun <forsyterun@gmail.com>
2023-12-06 16:02:19 +0200

    feat: add textBlock

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,7 +1,29 @@
-import { GraphiQL } from '../components';
-
 const Welcome = () => {
-  return <GraphiQL />;
+  return (
+    <div className="welcome-container">
+      <div>
+        <h1>GraphiQL is the ability to harness the full power of GraphQL</h1>
+        <span>make a request to your own universe</span>
+      </div>
+      <ul className="welcome-list">
+        <li>
+          <span>Efficiency:</span>Clients get exactly the data they need, no
+          more, no less
+        </li>
+        <li>
+          <span>Flexibility:</span>Clients can request multiple resources in a
+          single query
+        </li>
+        <li>
+          <span>Versioning:</span>No need for versioning as clients have control
+          over the data they receive
+        </li>
+        <li>
+          <span>Strongly Typed:</span>Type checking at the query level
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Welcome;
