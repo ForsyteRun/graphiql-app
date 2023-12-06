f624d02d9d38127480875f982c5452c92815c8f3
massaracsh7 <fatum7@gmail.com>
2023-12-06 11:07:31 +0300

    feat: add LoginForm

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,7 +1,60 @@
-// LoginForm.tsx
+import React from 'react';
+import { useForm } from 'react-hook-form';
 
-const LoginForm = () => {
-  return <></>;
+import { yupResolver } from '@hookform/resolvers/yup';
+
+import { DataLogin } from '../types/types';
+import { schemaLogin } from '../utils/schema';
+import { registerWithEmailAndPassword } from '../firebase/firebase';
+import { useNavigate } from 'react-router-dom';
+import { MAIN_ROUTE } from '../constants/route'; // LoginForm.tsx
+
+const LoginForm: React.FC = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<DataLogin>({
+    mode: 'onChange',
+    resolver: yupResolver(schemaLogin),
+  });
+
+  const navigate = useNavigate();
+
+  const onSubmit = (data: DataLogin) => {
+    console.log(data);
+    registerWithEmailAndPassword(data.email, data.password);
+    navigate(MAIN_ROUTE);
+  };
+
+  return (
+    <form className="form" onSubmit={handleSubmit(onSubmit)}>
+      <label>Email </label>
+      {errors.email && (
+        <span className="form__error">{errors.email.message}</span>
+      )}
+      <input
+        className="form__input"
+        id="email"
+        {...register('email')}
+        placeholder="email@gmail.com"
+      />
+
+      <label htmlFor="password">Password </label>
+      {errors.password && (
+        <span className="form__error">{errors.password.message}</span>
+      )}
+      <input
+        {...register('password')}
+        type="password"
+        id="password"
+        className="form__input"
+      />
+      <button className="form__btn" type="submit" disabled={!isValid}>
+        Submit
+      </button>
+    </form>
+  );
 };
 
 export { LoginForm };
--- src/utils/schema.tsx
+++ src/utils/schema.tsx
@@ -19,3 +19,19 @@ export const schema = yup.object().shape({
     .required()
     .oneOf([yup.ref('password')], ' Passwords must match'),
 });
+
+export const schemaLogin = yup.object().shape({
+  email: yup
+    .string()
+    .required(' is required field')
+    .email('please, enter a valid email (e.g., user@example.com)')
+    .max(255),
+  password: yup
+    .string()
+    .required(' is a required field')
+    .matches(/^(?=.*[a-z])/, 'Must Contain One Lowercased Character.')
+    .matches(/^(?=.*[A-Z])/, 'Must Contain One Uppercased Character.')
+    .matches(/^(?=.*[0-9])/, 'Must Contain One Number Character.')
+    .matches(/^(?=.*[!@#$%^&*])/, 'Must Contain  One Special Case Character.')
+    .min(8, 'Must Contain 8 Characters. '),
+});
