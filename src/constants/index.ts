a7c9ae27e8ef1caeb0b9d9237c31230054ffe7f2
ForsyteRun <forsyterun@gmail.com>
2023-12-07 08:40:25 +0200

    style: adaptive welcomePage -> content

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,6 @@
 export const welcomePageContentList = {
-  'Efficiency:': 'Clients get exactly the data they need, no more, no less',
-  'Flexibility:': 'Clients can request multiple resources in a single query',
-  'Versioning:': 'No need for versioning as clients have control',
-  'Strongly Typed:': 'Type checking at the query level',
+  Efficiency: 'Clients get exactly the data they need, no more, no less',
+  Flexibility: 'Clients can request multiple resources in a single query',
+  Versioning: 'No need for versioning as clients have control',
+  'Strongly Typed': 'Type checking at the query level',
 };
