7166a5d2abb58e1818b6e4c6be450651513f1d4c
massaracsh7 <fatum7@gmail.com>
2023-12-07 14:35:48 +0300

    feat: add store, userSlice

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './assets/scss/style.scss';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
+import { Provider } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import store from './store/store';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider
-      router={router}
-      // fallbackElement={<div className="lds-dual-ring"></div>}
-    />
+    <Provider store={store}>
+      <ToastContainer />
+      <RouterProvider
+        router={router}
+        // fallbackElement={<div className="lds-dual-ring"></div>}
+      />
+    </Provider>
   </React.StrictMode>
 );
