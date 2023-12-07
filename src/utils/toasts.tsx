deebcf3877d2906e030153a470d92baf1c4d608f
massaracsh7 <fatum7@gmail.com>
2023-12-07 14:25:59 +0300

    feat: add toasts

---
+++ src/utils/toasts.tsx
@@ -0,0 +1,71 @@
+import { toast } from 'react-toastify';
+import { FirebaseError } from 'firebase/app';
+import {
+  TOAST_LOG_OUT_PENDING,
+  TOAST_LOG_OUT_SUCCESS,
+  TOAST_NO_CONNECTION,
+  TOAST_SIGN_IN_PENDING,
+  TOAST_SIGN_IN_SUCCESS,
+  TOAST_SIGN_UP_PENDING,
+  TOAST_SIGN_UP_SUCCESS,
+} from '../constants/toastsConst';
+
+export const toastSignUp = async (
+  onRenderError: (error: FirebaseError) => string,
+  signUp: () => Promise<unknown> | (() => Promise<unknown>)
+) => {
+  const response = await toast.promise(signUp(), {
+    pending: TOAST_SIGN_UP_PENDING,
+    success: TOAST_SIGN_UP_SUCCESS,
+    error: {
+      render(error) {
+        const apiError = error as unknown as FirebaseError;
+        return onRenderError(apiError);
+      },
+    },
+  });
+  return response;
+};
+
+export const toastForNoConnection = () => {
+  if (!navigator.onLine) {
+    toast.error(TOAST_NO_CONNECTION);
+    return true;
+  }
+};
+
+export const toastSignIn = async (
+  onRenderError: (error: FirebaseError) => string,
+  signIn: () => Promise<unknown> | (() => Promise<unknown>)
+) => {
+  const response = await toast.promise(signIn(), {
+    pending: TOAST_SIGN_IN_PENDING,
+    success: TOAST_SIGN_IN_SUCCESS,
+    error: {
+      render(error) {
+        const apiError = error as unknown as FirebaseError;
+        return onRenderError(apiError);
+      },
+    },
+  });
+
+  return response;
+};
+
+export const toastLogout = async (
+  onRenderError: (error: FirebaseError) => string,
+  logout: () => Promise<unknown> | (() => Promise<unknown>)
+) => {
+  const response = await toast.promise(logout(), {
+    pending: TOAST_LOG_OUT_PENDING,
+    success: TOAST_LOG_OUT_SUCCESS,
+    error: {
+      render(error) {
+        const apiError = error as unknown as FirebaseError;
+        return onRenderError(apiError);
+      },
+    },
+  });
+
+  return response;
+};
