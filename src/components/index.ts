f3ab2b459a28962c3f88e02b6db2684321ea3a52
ForsyteRun <forsyterun@gmail.com>
2023-12-07 06:59:51 +0200

    refactor: ourTeam & Content

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,4 @@
-export { default as GraphiQL } from './GraphiQL';
 export { default as LoginForm } from './LoginForm';
 export { default as Logo } from './Logo';
-export { default as OurTeam } from './OurTeam';
-export { default as TeamMember } from './TeamMember';
 export { default as RegistrationForm } from './RegistrationForm';
+export * from './welcomePage';
