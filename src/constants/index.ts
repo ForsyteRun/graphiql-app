264dee665178da5331bf6c25d72572211162c569
ForsyteRun <forsyterun@gmail.com>
2023-12-07 07:13:38 +0200

    style: Welcome -> Content

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,6 @@
 export const welcomePageContentList = {
   'Efficiency:': 'Clients get exactly the data they need, no more, no less',
   'Flexibility:': 'Clients can request multiple resources in a single query',
-  'Versioning:':
-    'No need for versioning as clients have control over the data they receive',
+  'Versioning:': 'No need for versioning as clients have control',
   'Strongly Typed:': 'Type checking at the query level',
 };
--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -3,7 +3,7 @@ import { Content, OurTeam } from '../components';
 const Welcome = () => {
   return (
     <div className="welcome-container">
-      <div>
+      <div className="welcome-title">
         <h1>GraphiQL is the ability to harness the full power of GraphQL</h1>
         <span>make a request to your own universe</span>
       </div>
