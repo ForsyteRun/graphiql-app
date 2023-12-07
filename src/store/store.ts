f8146c85ab352ce96ebc5522faa056f5bac69f54
massaracsh7 <fatum7@gmail.com>
2023-12-07 14:35:48 +0300

    feat: add store, userSlice

---
+++ src/store/store.ts
@@ -0,0 +1,15 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/dist/query';
+import userReducer from './slice/userSlice';
+
+const reducer = combineReducers({
+  user: userReducer,
+});
+
+const store = configureStore({
+  reducer,
+});
+
+setupListeners(store.dispatch);
+
+export default store;
