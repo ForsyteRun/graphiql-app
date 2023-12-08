7aaab8a45b97980bd80060818980d25972042c69
ForsyteRun <forsyterun@gmail.com>
2023-12-08 18:02:02 +0200

    feat: add modal & button

--- src/components/Docs/components/Button.tsx
+++ src/components/Docs/components/Button.tsx
@@ -1,5 +1,15 @@
-const Button = () => {
-  return <button className="docs-button">Button</button>;
+const Button = ({
+  setOpen,
+  open,
+}: {
+  setOpen: (value: boolean) => void;
+  open: boolean;
+}) => {
+  return (
+    <button className="docs-button" onClick={() => setOpen(!open)}>
+      Docs
+    </button>
+  );
 };
 
 export default Button;
