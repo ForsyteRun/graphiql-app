27dc3792dcf85eca6c5b0a9ed94895b7f98083fd
ForsyteRun <forsyterun@gmail.com>
2023-12-08 18:02:02 +0200

    feat: add modal & button

--- src/components/Docs/index.tsx
+++ src/components/Docs/index.tsx
@@ -1,14 +1,16 @@
+import classNames from 'classnames';
+import useOpen from '../../hooks/useOpen';
 import { Button } from './components';
 
 const Docs = () => {
+  const { setOpen, open } = useOpen();
+
   return (
-    <aside>
-      <div className="docs-container">
-        qqqscscscscscscscscs qqqscscscscscscscscs qqqscscscscscscscscs
-        qqqscscscscscscscscs qqqscscscscscscscscs qqqscscscscscscscscs
-        qqqscscscscscscscscs
-        <Button />
-      </div>
+    <aside className={classNames('docs-container', { closeModal: open })}>
+      qqqscscscscscscscscs qqqscscscscscscscscs qqqscscscscscscscscs
+      qqqscscscscscscscscs qqqscscscscscscscscs qqqscscscscscscscscs
+      qqqscscscscscscscscs
+      <Button setOpen={setOpen} open={open} />
     </aside>
   );
 };
