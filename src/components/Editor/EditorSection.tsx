9bbedc4523adec85d431bbcdfa6d1c9d372f38c2
nyurasheva <n.yurasheva@gmail.com>
2023-12-08 12:13:47 +0300

    fix: number in editor

--- src/components/Editor/EditorSection.tsx
+++ src/components/Editor/EditorSection.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import { sectionData } from '../../constants/editor';
 
 interface EditorSectionProps {
@@ -5,21 +6,11 @@ interface EditorSectionProps {
 }
 
 const EditorSection: React.FC<EditorSectionProps> = ({ title }) => {
-  return (
-    <>
-      <div className="editor__header">
-        <div className="editor__title">{title}</div>
-      </div>
-      <div className="editor__content">
-        <div className="editor__numbers">
-          {[...Array(15).keys()].map((num) => (
-            <p key={num + 1}>{num + 1}</p>
-          ))}
-        </div>
-        <div className="editor__text">
-          {sectionData.query.label === title && (
-            <textarea className="editor__query">
-              {`query {
+  const countLines = (text: string) => {
+    return text.split('\n').length;
+  };
+
+  const initialQuery = `query {
   characters(page: 2, filter: { name: "rick" }) {
     info {
       count
@@ -34,9 +25,34 @@ const EditorSection: React.FC<EditorSectionProps> = ({ title }) => {
   episodesByIds(ids: [1, 2]) {
     id
   }
-}
-              `}
-            </textarea>
+}`;
+
+  const handleQueryChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const lines = countLines(event.target.value);
+    setNumLines(lines);
+  };
+
+  const [numLines, setNumLines] = useState(countLines(initialQuery));
+
+  return (
+    <>
+      <div className="editor__header">
+        <div className="editor__title">{title}</div>
+      </div>
+      <div className="editor__content">
+        <div className="editor__numbers">
+          {[...Array(numLines).keys()].map((num) => (
+            <p key={num + 1}>{num + 1}</p>
+          ))}
+        </div>
+        <div className="editor__text">
+          {sectionData.query.label === title && (
+            <textarea
+              className="editor__query"
+              value={initialQuery}
+              onChange={handleQueryChange}
+              rows={numLines}
+            />
           )}
         </div>
       </div>
