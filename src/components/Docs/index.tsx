453e2280129ab967f7430a6e5b08ea0848d26225
ForsyteRun <forsyterun@gmail.com>
2023-12-09 08:10:59 +0200

    fix: make input API controlled

--- src/components/Docs/index.tsx
+++ src/components/Docs/index.tsx
@@ -1,12 +1,19 @@
 import classNames from 'classnames';
 import useOpen from '../../hooks/useOpen';
 import { Button } from './components';
+import { useEffect } from 'react';
 
 const Docs = () => {
   const { setOpen, open } = useOpen();
 
+  useEffect(() => {
+    if (open) {
+      console.log(111);
+    }
+  }, [open]);
+
   return (
-    <aside className={classNames('docs-container', { closeModal: open })}>
+    <aside className={classNames('docs-container', { openModal: open })}>
       qqqscscscscscscscscs qqqscscscscscscscscs qqqscscscscscscscscs
       qqqscscscscscscscscs qqqscscscscscscscscs qqqscscscscscscscscs
       qqqscscscscscscscscs
