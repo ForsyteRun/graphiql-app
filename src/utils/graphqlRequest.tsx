83ff934a76091abd70cfdbbca9397c0e095f85fe
massaracsh7 <fatum7@gmail.com>
2023-12-10 12:10:04 +0300

    feat: add useGraphqlRequest

---
+++ src/utils/graphqlRequest.tsx
@@ -0,0 +1,25 @@
+import { useCallback, useEffect } from 'react';
+import { setResponse } from '../store/slice/requestSlice';
+import { useAppDispatch, useAppSelector } from '../store/types';
+
+export const useGraphqlRequest = (query: string) => {
+  const { api, variables, headers } = useAppSelector((state) => state.request);
+  const requestHeaders = headers as Headers;
+  const getData = useCallback(
+    async (query: string) => {
+      return await fetch(api, {
+        method: 'POST',
+        headers: requestHeaders,
+        body: JSON.stringify({ query, variables }),
+      }).then((data) => data.json());
+    },
+    [api, requestHeaders, variables]
+  );
+
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    getData(query).then((response) => {
+      dispatch(setResponse(JSON.stringify(response)));
+    });
+  }, [dispatch, getData, query]);
+};
