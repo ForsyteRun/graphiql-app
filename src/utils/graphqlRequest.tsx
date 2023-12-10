76079a56c366da3ce4951161c26040561bbea124
massaracsh7 <fatum7@gmail.com>
2023-12-10 21:31:57 +0300

    feat: add checkVariables

--- src/utils/graphqlRequest.tsx
+++ src/utils/graphqlRequest.tsx
@@ -1,19 +1,25 @@
 import { useCallback, useEffect } from 'react';
 import { setResponse } from '../store/slice/requestSlice';
 import { useAppDispatch, useAppSelector } from '../store/types';
+import { checkVariables } from './checkVariables';
 
 export const useGraphqlRequest = () => {
   const { api, variables, headers, query } = useAppSelector(
     (state) => state.request
   );
   const requestHeaders = headers as Headers;
-  const ver = variables ? JSON.parse(variables) : '';
+
   const getData = useCallback(
-    async (query: string, ver: string, api: string, requestHeaders: object) => {
+    async (
+      query: string,
+      variables: string,
+      api: string,
+      requestHeaders: object
+    ) => {
       return await fetch(api, {
         method: 'POST',
         headers: requestHeaders as Headers,
-        body: JSON.stringify({ query, variables: ver }),
+        body: JSON.stringify({ query, variables: checkVariables(variables) }),
       }).then((data) => data.json());
     },
     []
@@ -21,9 +27,9 @@ export const useGraphqlRequest = () => {
 
   const dispatch = useAppDispatch();
   useEffect(() => {
-    getData(query, ver, api, requestHeaders).then((response) => {
+    getData(query, variables, api, requestHeaders).then((response) => {
       console.log(response);
       dispatch(setResponse(JSON.stringify(response, null, 2)));
     });
-  }, [dispatch, getData, query, api, requestHeaders, ver]);
+  }, [dispatch, getData, query, api, requestHeaders, variables]);
 };
