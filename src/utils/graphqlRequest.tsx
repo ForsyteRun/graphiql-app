20090f14b56c6db5810f0c4fb5b00ae7a1cf8726
massaracsh7 <fatum7@gmail.com>
2023-12-10 16:32:02 +0300

    refactor: update graphqlReuest

--- src/utils/graphqlRequest.tsx
+++ src/utils/graphqlRequest.tsx
@@ -2,24 +2,32 @@ import { useCallback, useEffect } from 'react';
 import { setResponse } from '../store/slice/requestSlice';
 import { useAppDispatch, useAppSelector } from '../store/types';
 
-export const useGraphqlRequest = (query: string) => {
-  const { api, variables, headers } = useAppSelector((state) => state.request);
+export const useGraphqlRequest = () => {
+  const { api, variables, headers, query } = useAppSelector(
+    (state) => state.request
+  );
   const requestHeaders = headers as Headers;
   const getData = useCallback(
-    async (query: string) => {
+    async (
+      query: string,
+      variables: string,
+      api: string,
+      requestHeaders: object
+    ) => {
       return await fetch(api, {
         method: 'POST',
-        headers: requestHeaders,
-        body: JSON.stringify({ query, variables }),
+        headers: requestHeaders as Headers,
+        body: JSON.stringify({ query: query, variables: variables }),
       }).then((data) => data.json());
     },
-    [api, requestHeaders, variables]
+    []
   );
 
   const dispatch = useAppDispatch();
   useEffect(() => {
-    getData(query).then((response) => {
+    getData(query, variables, api, requestHeaders).then((response) => {
+      console.log(response);
       dispatch(setResponse(JSON.stringify(response, null, 2)));
     });
-  }, [dispatch, getData, query]);
+  }, [dispatch, getData, query, api, requestHeaders, variables]);
 };
