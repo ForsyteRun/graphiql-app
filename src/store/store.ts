e92f05ff574aa9bdc3d2daede3020a3611b42b93
massaracsh7 <fatum7@gmail.com>
2023-12-10 12:00:40 +0300

    feat: add requestSlice

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,11 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query';
 import userReducer from './slice/userSlice';
+import requestReducer from './slice/requestSlice';
 
 const reducer = combineReducers({
   user: userReducer,
+  request: requestReducer,
 });
 
 const store = configureStore({
