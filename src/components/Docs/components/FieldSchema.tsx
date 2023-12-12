392013034084c1d479b0c3d3be50cef4cb5108b5
ForsyteRun <forsyterun@gmail.com>
2023-12-12 14:24:09 +0200

    fix: condition  FieldSchema

--- src/components/Docs/components/FieldSchema.tsx
+++ src/components/Docs/components/FieldSchema.tsx
@@ -53,6 +53,30 @@ const FieldSchema = memo(({ schema, setRootSchema }: IFieldSchema) => {
         const modiFyData: IRootSchema = {
           fields: { [value.name]: { ...value } } as unknown as FieldsType,
         };
+        setRootSchema(modiFyData);
+        setIsDescription(false);
+      } else if (isLeafType(value)) {
+        const modiFyData: IRootSchema = {
+          fields: { [value.name]: { ...value } } as unknown as FieldsType,
+        };
+        setRootSchema(modiFyData);
+        setIsDescription(false);
+      } else if (isLeafType(value)) {
+        const modiFyData: IRootSchema = {
+          fields: { [value.name]: { ...value } } as unknown as FieldsType,
+        };
+        setRootSchema(modiFyData);
+        setIsDescription(false);
+      } else if (isLeafType(value)) {
+        const modiFyData: IRootSchema = {
+          fields: { [value.name]: { ...value } } as unknown as FieldsType,
+        };
+        setRootSchema(modiFyData);
+        setIsDescription(false);
+      } else if (isLeafType(value)) {
+        const modiFyData: IRootSchema = {
+          fields: { [value.name]: { ...value } } as unknown as FieldsType,
+        };
 
         setRootSchema(modiFyData);
         setIsDescription(true);
