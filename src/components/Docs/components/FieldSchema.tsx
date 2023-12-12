f2cd6abba26857e5a777b2782f75e55b82eee6e2
ForsyteRun <forsyterun@gmail.com>
2023-12-12 14:24:09 +0200

    fix: condition  FieldSchema

--- src/components/Docs/components/FieldSchema.tsx
+++ src/components/Docs/components/FieldSchema.tsx
@@ -41,6 +41,18 @@ const FieldSchema = memo(({ schema, setRootSchema }: IFieldSchema) => {
         const modiFyData: IRootSchema = {
           fields: { [value.name]: { ...value } } as unknown as FieldsType,
         };
+        setRootSchema(modiFyData);
+        setIsDescription(false);
+      } else if (isLeafType(value)) {
+        const modiFyData: IRootSchema = {
+          fields: { [value.name]: { ...value } } as unknown as FieldsType,
+        };
+        setRootSchema(modiFyData);
+        setIsDescription(false);
+      } else if (isLeafType(value)) {
+        const modiFyData: IRootSchema = {
+          fields: { [value.name]: { ...value } } as unknown as FieldsType,
+        };
 
         setRootSchema(modiFyData);
         setIsDescription(true);
