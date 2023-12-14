63b44edf1378bb16b5a20af2bac72293ff8c4815
nyurasheva <n.yurasheva@gmail.com>
2023-12-14 21:41:35 +0300

    feat: add style registration

--- src/components/RegistrationForm.tsx
+++ src/components/RegistrationForm.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
-
 import { yupResolver } from '@hookform/resolvers/yup';
-import { DataForm } from '../types/types';
+import { DataForm, FieldName } from '../types/types';
 import { schema } from '../utils/schema';
 import { registerWithEmailAndPassword } from '../firebase/firebase';
 import { toastForNoConnection, toastSignUp } from '../utils/toasts';
@@ -15,6 +14,7 @@ import { MAIN_ROUTE } from '../constants/route';
 import { useNavigate } from 'react-router-dom';
 import { useAppDispatch } from '../store/types';
 import { setIsLogin } from '../store/slice/userSlice';
+import { fields } from '../constants/fields';
 
 const onRenderError = (error: FirebaseError) => {
   if (error.code === 'auth/email-already-in-use') {
@@ -31,7 +31,7 @@ const RegistrationForm: React.FC = () => {
     register,
     handleSubmit,
     reset,
-    formState: { errors, isValid },
+    formState: { errors },
   } = useForm<DataForm>({
     mode: 'onChange',
     resolver: yupResolver(schema),
@@ -55,40 +55,29 @@ const RegistrationForm: React.FC = () => {
 
   return (
     <form className="form" onSubmit={handleSubmit(onSubmit)}>
-      <label>Email </label>
-      {errors.email && (
-        <span className="form__error">{errors.email.message}</span>
-      )}
-      <input
-        className="form__input"
-        id="email"
-        {...register('email')}
-        placeholder="email@gmail.com"
-      />
+      {fields.map(({ label, name, type, placeholder }) => (
+        <label
+          key={name}
+          className={`label ${errors[name as FieldName] ? 'error' : ''}`}
+          htmlFor={name}
+        >
+          <span>{label}</span>
+          <input
+            {...register(name as FieldName)}
+            type={type}
+            id={name}
+            placeholder={placeholder}
+          />
+          {errors[name as FieldName] && (
+            <div className="error-message">
+              {errors[name as FieldName]?.message}
+            </div>
+          )}
+        </label>
+      ))}
 
-      <label htmlFor="password">Password </label>
-      {errors.password && (
-        <span className="form__error">{errors.password.message}</span>
-      )}
-      <input
-        {...register('password')}
-        type="password"
-        id="password"
-        className="form__input"
-      />
-
-      <label htmlFor="confirmPassword">Confirm Password </label>
-      {errors.confirmPassword && (
-        <span className="form__error">{errors.confirmPassword?.message}</span>
-      )}
-      <input
-        type="password"
-        {...register('confirmPassword')}
-        id="conformPassword"
-        className="form__input"
-      />
-      <button className="form__btn" type="submit" disabled={!isValid}>
-        Submit
+      <button className="button button-second" type="submit">
+        Отправить
       </button>
     </form>
   );
