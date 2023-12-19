bf10084962ca63efd060bda0a6d9c83befb3f4bf
massaracsh7 <fatum7@gmail.com>
2023-12-19 14:00:51 +0300

    feat: add localContext

---
+++ src/context/LocalContext.tsx
@@ -0,0 +1,50 @@
+import React, {
+  createContext,
+  useState,
+  useEffect,
+  useContext,
+  ReactNode,
+} from 'react';
+import { LocalizationContextType, Translation } from './types';
+import { translationsData } from './LocalData';
+
+const LocalizationContext = createContext<LocalizationContextType | undefined>(
+  undefined
+);
+
+type LocalizationProviderProps = {
+  children: ReactNode;
+};
+
+export const LocalizationProvider: React.FC<LocalizationProviderProps> = ({
+  children,
+}) => {
+  const [language, setLanguage] = useState<string>('en');
+  const [translations, setTranslations] = useState<Translation>({});
+
+  const changeLanguage = (newLanguage: string) => {
+    setLanguage(newLanguage);
+  };
+
+  useEffect(() => {
+    setTranslations(translationsData[language]);
+  }, [language]);
+
+  return (
+    <LocalizationContext.Provider
+      value={{ language, translations, changeLanguage }}
+    >
+      {children}
+    </LocalizationContext.Provider>
+  );
+};
+
+export const useLocalization = (): LocalizationContextType => {
+  const context = useContext(LocalizationContext);
+  if (!context) {
+    throw new Error(
+      'useLocalization must be used within a LocalizationProvider'
+    );
+  }
+  return context;
+};
