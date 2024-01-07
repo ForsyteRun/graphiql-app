5bc0eac9f4cd79837e5bdda103d3d269b1c3edf4
nyurasheva <n.yurasheva@gmail.com>
2024-01-07 16:42:34 +0300

    fix: about RS School

--- src/context/LocalData.ts
+++ src/context/LocalData.ts
@@ -31,8 +31,9 @@ export const translationsData: Translations = {
     graphqlDescr:
       'The task is to implement a playground/IDE for working with GraphQL queries. GraphQL is a query language used by client applications to handle data. GraphQL is associated with the concept of a schema that allows organizing the creation, reading, updating, and deleting of data in the application. GraphiQL is an open-source tool. In addition to the IDE itself for working with GraphQL queries, authorization capabilities are implemented to grant access to the tool only to authorized users. ',
     dogTitle: `Let's do something together!`,
-    dogText: 'Try using our editor.',
-    dogLink: 'Meet the team',
+    dogText1: 'Try to use our editor created thanks to the course ',
+    dogText2:
+      'By taking courses, you get: free training, technological materials, a mentor, new friends, a certificate and much more.',
     dogBtn: 'Go to the editor',
     aboutTitle: 'You get it!',
     plusTitle: 'Advantages',
@@ -73,8 +74,10 @@ export const translationsData: Translations = {
     graphqlDescr:
       'Задача состоит в том, чтобы реализовать playground/IDE для работы с запросами GraphQL.GraphQL - это язык запросов, используемый клиентскими приложениями для обработки данных.GraphQL ассоциируется с таким понятием, как schema, которое позволяет организовать создание, чтение, обновление и удаление данных в приложении. GraphiQL - это инструмент с открытым исходным кодом.Помимо самого ide для работы с запросами GraphQL, реализованы возможности авторизации, чтобы предоставить доступ к инструменту только авторизованным пользователям. ',
     dogTitle: 'Давайте делать что-нибудь вместе!',
-    dogText: 'Попробуйте в использовании наш редактор.',
-    dogLink: 'Познакомиться с командой',
+    dogText1:
+      'Попробуйте в использовании наш редактор, созданный благодаря курсу от ',
+    dogText2:
+      'Обучаясь на курсе вы получаете: бесплатное обучение, учебные материалы, ментора, новых друзей, сертификат и многое другое.',
     dogBtn: 'Перейти в редактор',
     aboutTitle: 'Вы получаете!',
     plusTitle: 'Преимущества',
